a4cacf4ba46604666ce999eda1513d322cd07d7d
nikkuts <nickkuts888@gmail.com>
2023-05-02 00:28:12 +0300

    up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,45 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import React, { Component } from "react";
+import { nanoid } from 'nanoid';
+import Phonebook from "./Phonebook/Phonebook";
+
+class App extends Component {
+  state = {
+    contacts: [],
+    name: ''
+  }
+
+  handleChange = e => {
+    this.setState({ name: e.target.value }); 
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    
+    const arrayContacts = this.state.contacts;
+    arrayContacts.push({
+      name: this.state.name,
+      id: nanoid(),
+    });
+
+    this.setState({ contacts: arrayContacts}); 
+    e.currentTarget.reset();
+  };
+
+  render () {
+    const {contacts, name} = this.state;
+
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <Phonebook 
+        name={name}
+        onChange={this.handleChange}
+        onSubmit={this.handleSubmit}
+        /> 
+      </div>
+    );
+  }
+  
 };
+
+export default App;
